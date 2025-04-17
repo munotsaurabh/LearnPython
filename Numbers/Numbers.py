29f424ca5ad430b33ddf1551a4f9471565e41da1
Saurabh Munot <saurabhmunot@Saurabhs-MacBook-Pro.local>
2025-04-17 14:56:13 +0530

    Numbers example

---
+++ Numbers/Numbers.py
@@ -0,0 +1,30 @@
+from math import *
+
+print (10 % 3)          # prints remainder
+print (10 // 3)         # floor division of the number meaning it divides the number and prints the nearest whole number
+print (10 / 3)          # divides the number and gives a floating point quotient
+
+my_num=23
+print (my_num)
+print (str(my_num)+ " is my number")
+
+second_num= -5
+print(abs(second_num))  # returns the absolute value of a number means removes the negative sign
+print (pow(second_num,2))
+
+third_num=7
+print (pow(third_num,2))
+print (pow(third_num,4))
+
+print (max(third_num,second_num))
+print (min(third_num,second_num))
+
+print (round(3.499))
+print (round(3.5))
+print (round(3.6))
+print(f"this {round(2.5)}")
+
+print (floor(3.6))
+print (ceil(2.4))
+
+print (sqrt(49))
