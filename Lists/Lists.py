4deea474b13442c330d0c832fa630ec444bff8de
Saurabh Munot <saurabhmunot@Saurabhs-MacBook-Pro.local>
2025-04-24 18:40:56 +0530

    More List examples

--- Lists/Lists.py
+++ Lists/Lists.py
@@ -4,8 +4,8 @@ print (friends[0])
 ########################################
 
 list_in_list = [
-  ["Virat", 24], 
-  ["Hardik", 20], 
+  ["Virat", 24],
+  ["Hardik", 20],
   ["Bhuvi", 30]
   ]
 print (list_in_list[1]) #Prints the entire second element of the outer list i.e. ['Sam', 20]
@@ -31,3 +31,43 @@ Joining a list example
 friends = ["Mark", "Sam", "John"]
 comma_separated = ", ".join(friends)
 print(f"My friends are {comma_separated}.")
+
+########################################
+players = ["Lionel", "Cristiano", "Paul", "Kylian", "Eden"]
+numbers = [10, 7, 11, 9, 1]
+players_new = ["Lionel", "Cristiano", "Paul", "Paul", "Kylian", "Eden"]
+
+players.append("Luka")
+print(players)
+
+players.insert(2, "Vinicius")
+print(players)
+
+numbers.remove(9)
+print(numbers)
+
+numbers.pop()
+print(numbers)
+
+print(players.index("Kylian"))
+
+print(players_new.count("Paul"))
+
+players.sort()
+print(players)
+
+numbers.sort()
+print(numbers)
+
+players.reverse()
+print(players)
+
+players2 = players.copy()
+print(players2)
+
+
+players.extend(numbers)
+print(players)
+
+numbers.clear()
+print(numbers)
